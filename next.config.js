eafda979a0d895ac6e5526136416de608a0a530c
pacecapi <pablocaicedo@gmail.com>
2026-01-13 19:15:54 +0100

    Fix root page redirect to index.html

--- next.config.js
+++ next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
